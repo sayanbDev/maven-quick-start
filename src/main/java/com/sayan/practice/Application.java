1b0cc3f53db160881ca990c490c87b71748fa0be
Sayan Banerjee <banerjeesayan@gmail.com>
2022-11-01 16:15:37 +0530

    Adding application.java

---
+++ src/main/java/com/sayan/practice/Application.java
@@ -0,0 +1,14 @@
+package com.sayan.practice;
+
+class Application {
+
+    public Application() {
+        System.out.println("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main(String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+    }
+}
